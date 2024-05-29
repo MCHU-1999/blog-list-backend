0a1ae7163491df2827c39d8c98ffa14ef8d02ac0
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-29 17:28:18 +0800

    INIT & 4.1 Blog List, step 1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # blog-list-backend
 A repository for the Fullstackopen courses part4 (blog list backend)
+
+Example project structure:  
+```
+├── index.js  
+├── app.js  
+├── dist  
+│   └── ...  
+├── controllers  
+│   └── notes.js  
+├── models  
+│   └── note.js  
+├── package-lock.json  
+├── package.json  
+├── utils  
+│   ├── config.js  
+│   ├── logger.js  
+│   └── middleware.js    
+```
---
+++ app.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+
+// establish a connection to database
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to mongo DB')
+  })
+  .catch((error) => {
+    logger.info('error connecting mongo DB', error.message)
+  })
+
+const app = express()
+
+// preprocess
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+// routers
+app.use('/api/blogs', blogRouter)
+
+// unknown routes and error handling
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
