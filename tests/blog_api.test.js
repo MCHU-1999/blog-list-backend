38f028234917b45a891560297a8fcb97cb75e824
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-30 21:17:13 +0800

    4.9: Blog List Tests, step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -37,6 +37,21 @@ describe('blog tests', () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, listWithManyBlogs.length)
   })
+
+  test('index key is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    const idCount = _.countBy(response.body, 'id')
+
+    const reduced = response.body.reduce((acc, value) => {
+      if (value.id) {
+        return acc + 1
+      } else {
+        return acc
+      }
+    }, 0)
+    // console.log(reduced)
+    assert.strictEqual(reduced, listWithManyBlogs.length)
+  })
 })
 
 
